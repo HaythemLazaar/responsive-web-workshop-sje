02d7883a432e93f874fe31792d8c8fe883cb4401
HaythemLazaar <haythem.lazaar1@gmail.com>
2022-12-14 18:22:04 +0100

    script fixed

--- assets/script.js
+++ assets/script.js
@@ -1,22 +1,23 @@
-const hamburger = document.querySelector(".hamburger")
-const menu = document.querySelector(".menu")
-const closeIcon = document.querySelector(".closeIcon")
-const menuIcon = document.querySelector(".menuIcon")
-const overlay = document.querySelector(".overlay")
+const menu = document.querySelector(".menu");
+const hamburger= document.querySelector(".hamburger");
+const closeIcon= document.querySelector(".closeIcon");
+const menuIcon = document.querySelector(".menuIcon");
+const overlay = document.querySelector(".overlay");
 
-function toggleMenu(){
-    if(menu.classList.contains(".showMenu")){
-        menu.classList.remove(".showMenu")
-        menuIcon.style.display = "block"
-        closeIcon.style.display = "none"
-        overlay.style.display = "none"
-    }else{
-        menu.classList.add(".showMenu")
-        menuIcon.style.display = "none"
-        closeIcon.style.display = "block"
-        overlay.style.display = "block"
-    }
+function toggleMenu() {
+  if (menu.classList.contains("showMenu")) {
+    menu.classList.remove("showMenu");
+    closeIcon.style.display = "none";
+    menuIcon.style.display = "block";
+    overlay.style.display = "none"
+    hamburger.style.position = "absolute";
+  } else {
+    menu.classList.add("showMenu");
+    closeIcon.style.display = "block";
+    menuIcon.style.display = "none";
+    overlay.style.display = "block"
+    hamburger.style.position = "fixed";
+  }
 }
 
-
-hamburger.addEventListener("click", toggleMenu())
+hamburger.addEventListener("click", toggleMenu);
